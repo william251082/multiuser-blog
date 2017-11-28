93904245e30b45120f91152a34c608f35f34bc33
William <williamdelrosario@yahoo.com>
2017-11-28 14:17:26 +0000

    Added Admin Files

---
+++ resources/views/includes/admin-header.blade.php
@@ -0,0 +1,12 @@
+<header class="top-nav">
+    <nav>
+        <ul>
+            <li> <a href="">Dashboard</a></li>
+            <li> <a href="">Posts</a></li>
+            <li> <a href="">Categories</a></li>
+            <li> <a href="">Contact Messages</a></li>
+            <li> <a href="">Logout</a></li>
+        </ul>
+    </nav>   
+    
+</header>
---
+++ resources/views/layouts/admin-master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Admin area</title>
+        <link rel="stylsheet" href={{ URL::secure('src/css/admin.css') }}>
+        @yield('styles')
+        </head>
+        <body>
+            @include('includes.admin-header')
+            @yield('content')
+            
+            <script type="text/javascript">
+                var baseUrl = "{{ URL::to('/') }}";
+            </script>
+            
+            @yield('scripts')
+        </body>
+    
+</html>
